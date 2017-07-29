a4e102c0effb8a22a99107bf065f639990794116
Максим Толстик <maxim.tolstik@gmail.com>
2017-07-29 11:42:28 +0300

    my first commit

---
+++ src/main/java/maks/sample/controller/IndexController.java
@@ -0,0 +1,23 @@
+package maks.sample.controller;
+
+import maks.sample.entity.Greeting;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class IndexController
+{
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
